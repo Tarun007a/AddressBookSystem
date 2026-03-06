b16f2188ba76abef2fbae4b87dbb93827343c041
Tarun007a <tarunchourey16@gmail.com>
2026-03-06 15:19:54 +0530

    [Tarun] : Added feature to ensure there is no duplicate contact with name

--- src/main/java/com/example/demo/model/Contact.java
+++ src/main/java/com/example/demo/model/Contact.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,4 +18,18 @@ public class Contact {
     private String zip;
     private String phoneNumber;
     private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contact)) return false;
+        Contact contact = (Contact) o;
+        return firstName.equalsIgnoreCase(contact.firstName) &&
+                lastName.equalsIgnoreCase(contact.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName.toLowerCase(), lastName.toLowerCase());
+    }
 }
