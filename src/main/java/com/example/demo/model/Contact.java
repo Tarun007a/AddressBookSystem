43129f7937b1b03357f1dbaa51c8b465e9bbcc6a
Tarun007a <tarunchourey16@gmail.com>
2026-03-07 11:48:56 +0530

    [Tarun] : Added feature to read and write from file

--- src/main/java/com/example/demo/model/Contact.java
+++ src/main/java/com/example/demo/model/Contact.java
@@ -1,14 +1,7 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Objects;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Contact {
     private String firstName;
     private String lastName;
@@ -19,11 +12,104 @@ public class Contact {
     private String phoneNumber;
     private String email;
 
+    public Contact() {
+    }
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String zip,
+                   String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName +
+                ", Address: " + address +
+                ", City: " + city +
+                ", State: " + state +
+                ", Zip: " + zip +
+                ", Phone: " + phoneNumber +
+                ", Email: " + email;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Contact)) return false;
+
         Contact contact = (Contact) o;
+
         return firstName.equalsIgnoreCase(contact.firstName) &&
                 lastName.equalsIgnoreCase(contact.lastName);
     }
